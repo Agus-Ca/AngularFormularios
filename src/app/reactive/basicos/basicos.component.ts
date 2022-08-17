68e0dcfb98224ca5a17bf058781b446dd6fd1115
Agustín Casado <agu.e.casado@gmail.com>
2022-08-17 01:35:28 -0300

    formularios reactivos basicos

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, /*FormControl,*/ FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -8,9 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicosComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private formBuilder:FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.miFormulario.reset({
+      nombre      : 'GTX 1660Super',
+      precio      : 80000,
+      existencias : 10
+    })
   }
 
+  // miFormulario:FormGroup = new FormGroup({
+  //   nombre      : new FormControl('RTX 3090'),
+  //   precio      : new FormControl(1500),
+  //   existencias : new FormControl(5),
+  // })
+
+  miFormulario: FormGroup = this.formBuilder.group({
+    nombre      : [ , [Validators.required, Validators.minLength(3)] ],
+    precio      : [ , [Validators.required, Validators.min(0)] ],
+    existencias : [ , [Validators.required, Validators.min(0)] ],
+  });
+
+  campoNoEsValido( campo:string ) {
+    return this.miFormulario?.get(campo)?.errors &&
+           this.miFormulario?.get(campo)?.touched;
+  }
+
+  guardar() {
+    if(this.miFormulario.invalid) {
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.miFormulario.value);
+    this.miFormulario.reset();
+  }
 }
