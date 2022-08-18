2abd172a9751ddfe3bd77a06ad44b96a77496030
Agustín Casado <agu.e.casado@gmail.com>
2022-08-18 01:43:21 -0300

    switches en formularios reactivos

--- src/app/reactive/switches/switches.component.ts
+++ src/app/reactive/switches/switches.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
@@ -8,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SwitchesComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.miFormulario.reset({
+      ...this.persona,
+      condiciones: true
+    });
+
+    this.miFormulario.valueChanges.subscribe( ({condiciones, ...rest}) => {
+      this.persona = rest;
+    })
   }
 
+  miFormulario: FormGroup = this.formBuilder.group({
+    genero: [ 'M', Validators.required ],
+    notificaciones: [ true, Validators.required ],
+    condiciones: [ false, [Validators.required, Validators.requiredTrue] ]
+  });
+
+  persona = {
+    genero: 'F',
+    notificaciones: true,
+  }
+
+  guardar() {
+    const formValue = {...this.miFormulario.value};
+    delete formValue.condiciones;
+
+    this.persona = formValue;
+  }
 }
