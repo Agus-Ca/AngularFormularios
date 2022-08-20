6303974b023b6afdcafbf73190ef85982cb154e4
Agustín Casado <agu.e.casado@gmail.com>
2022-08-20 18:26:20 -0300

    validacion nombre

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -8,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit(): void {
   }
 
+  nombreApellidoPattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+
+  miFormulario: FormGroup = this.formBuilder.group({
+    nombre: ['', [ Validators.required, Validators.pattern(this.nombreApellidoPattern) ]],
+  })
+
+  campoNoValido( campo:string ) {
+    return this.miFormulario.get(campo)?.invalid
+           && this.miFormulario.get(campo)?.touched;
+  }
+
+  submitFormulario() {
+    this.miFormulario.markAllAsTouched();
+  }
 }
