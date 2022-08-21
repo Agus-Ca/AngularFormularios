5470a380475204b77b671a590c25a47f54911cad
Agustín Casado <agu.e.casado@gmail.com>
2022-08-21 01:27:00 -0300

    validaciones reactivas del username

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -16,7 +16,8 @@ export class RegistroComponent implements OnInit {
   ngOnInit(): void {
     this.miFormulario.reset({
       nombre: 'Agustin Casado',
-      email: 'agu.e.casado@gmail.com'
+      email: 'agu.e.casado@gmail.com',
+      username: 'Agus'
     })
   }
 
@@ -24,13 +25,24 @@ export class RegistroComponent implements OnInit {
   emailPattern: string = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
 
   miFormulario: FormGroup = this.formBuilder.group({
-    nombre: ['', [ Validators.required, Validators.pattern(this.nombreApellidoPattern) ]],
-    email:  ['', [ Validators.required, Validators.email, Validators.pattern(this.emailPattern)]]
+    nombre:   ['', [ Validators.required, Validators.pattern(this.nombreApellidoPattern) ]],
+    email:    ['', [ Validators.required, Validators.email, Validators.pattern(this.emailPattern)]],
+    username: ['', [ Validators.required, this.noPuedeSerAgusCa ]]
   })
 
   campoNoValido( campo:string ) {
     return this.miFormulario.get(campo)?.invalid
            && this.miFormulario.get(campo)?.touched;
+  }
+
+  noPuedeSerAgusCa( control:FormControl ) {
+    const valor = control.value?.trim().toLowerCase();
+    if ( valor === 'AgusCa'.toLowerCase()) {
+      return {
+        noAgusCa: true
+      }
+    }
+    return null;
   }
 
   submitFormulario() {
